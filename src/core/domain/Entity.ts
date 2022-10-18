dfff0d221dad40b5bd0100091c661d30eb97f30d
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-10-18 16:59:32 -0300

    feat: add entity int core

---
+++ src/core/domain/Entity.ts
@@ -0,0 +1,9 @@
+export abstract class Entity <T>{
+    protected _id:string;
+    public props: T;
+
+    constructor(props: T, id?:string) {
+        this._id = id ?? crypto.randomUUID().toString();
+        this.props = props;
+    }
+}
