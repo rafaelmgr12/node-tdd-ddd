1c3e66f09c86d392d9e3d738d074633a52cbbd29
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-10-20 18:36:42 -0300

    fix: test fixes

--- src/application/usecase/create-challenge-submission.spec.ts
+++ src/application/usecase/create-challenge-submission.spec.ts
@@ -1,8 +1,13 @@
+import { InMemoryChallengesRepository } from "../../test/repositories/in-memory-challenges-repository";
+import { InMemoryStudentsRepository } from "../../test/repositories/in-memory-students-repository";
 import { CreateChallengeSubmission } from "./create-challenge-submission";
 
 describe("Create Challenge Submission use case", () => {
   it("should create a new challenge submission", async () => {
-    const sut = new CreateChallengeSubmission();
+    const studentRepository = new InMemoryStudentsRepository()
+    const challengeRepository = new InMemoryChallengesRepository()
+    
+    const sut = new CreateChallengeSubmission(studentRepository, challengeRepository);
 
     const response = await sut.execute({
       studentId: "any_student_id",
