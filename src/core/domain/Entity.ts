6cd8e516645f637f6f138cfc26cbf6da18df9de4
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-10-20 18:24:33 -0300

    feat : add getter for id

--- src/core/domain/Entity.ts
+++ src/core/domain/Entity.ts
@@ -5,6 +5,10 @@ export abstract class Entity <T>{
     protected _id:string;
     public props: T;
 
+    get id():string {
+        return this._id;
+    }
+
     constructor(props: T, id?:string) {
         this._id = id ?? crypto.randomUUID().toString();
         this.props = props;
