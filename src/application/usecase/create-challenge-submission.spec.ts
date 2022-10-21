2c3da14094ac9a5e1f618cb928affefa8fa5159b
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-10-21 11:47:29 -0300

    test: fix test for create challenge

--- src/application/usecase/create-challenge-submission.spec.ts
+++ src/application/usecase/create-challenge-submission.spec.ts
@@ -1,17 +1,35 @@
+import { Challenge } from "../../domain/entities/challenge";
+import { Student } from "../../domain/entities/student";
 import { InMemoryChallengesRepository } from "../../test/repositories/in-memory-challenges-repository";
 import { InMemoryStudentsRepository } from "../../test/repositories/in-memory-students-repository";
 import { CreateChallengeSubmission } from "./create-challenge-submission";
 
 describe("Create Challenge Submission use case", () => {
   it("should create a new challenge submission", async () => {
-    const studentRepository = new InMemoryStudentsRepository()
-    const challengeRepository = new InMemoryChallengesRepository()
-    
-    const sut = new CreateChallengeSubmission(studentRepository, challengeRepository);
+    const studentRepository = new InMemoryStudentsRepository();
+    const challengeRepository = new InMemoryChallengesRepository();
+
+    const student = Student.create({
+      name: "John Doe",
+      email: "doe@example.com",
+    });
+
+    const challenge = Challenge.create({
+      title: "Test Challenge",
+      instructionsUrl: "Test instructions",
+    });
+
+    studentRepository.items.push(student);
+    challengeRepository.items.push(challenge);
+
+    const sut = new CreateChallengeSubmission(
+      studentRepository,
+      challengeRepository
+    );
 
     const response = await sut.execute({
-      studentId: "any_student_id",
-      challengeId: "any_challenge_id",
+      studentId: student.id,
+      challengeId: challenge.id,
     });
 
     expect(response).toBeTruthy();
